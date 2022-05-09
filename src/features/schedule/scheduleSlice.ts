779808c6b1df8782eec59ddf46fe3196fde0a587
richardSHkim <kshtrue95@gmail.com>
2022-05-09 18:36:10 +0900

    schedule slice

---
+++ src/features/schedule/scheduleSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../store'
+
+// Define a type for the slice state
+interface ScheduleState {
+  schedule: number[]
+}
+
+// Define the initial state using that type
+const initialState: ScheduleState = {
+  schedule: [],
+}
+
+export const scheduleSlice = createSlice({
+  name: 'schedule',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {}
+})
+
+// export const {  } = scheduleSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+// export const selectSchedule = (state: RootState) => state.schedule.value
+
+export default scheduleSlice.reducer
