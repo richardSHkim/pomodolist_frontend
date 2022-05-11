77f6724f13da8ff419b592d6109b10fabc4064af
richardSHkim <kshtrue95@gmail.com>
2022-05-11 13:48:28 +0900

    can choose period

--- src/components/schedule/ScheduleItem.tsx
+++ src/components/schedule/ScheduleItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useAppSelector } from '../../hooks'
+import { setPeriod } from '../../features/schedule/scheduleSlice'
+import { useAppDispatch, useAppSelector } from '../../hooks'
 
 interface Props {
   item: number,
@@ -8,16 +9,22 @@ interface Props {
 
 const ScheduleItem = ({ item, index }: Props) => {
   const { period } = useAppSelector((state) => state.schedule)
+  const dispatch = useAppDispatch()
+
+  const handleOnClick = () => {
+    dispatch(setPeriod(index))
+  }
+
   if (index === period) {
     return (
-      <span>
+      <span onClick={handleOnClick}>
         {item} *
       </span>
     )
   }
   else {
     return (
-      <span>
+      <span onClick={handleOnClick}>
         {item}
       </span>
     )
