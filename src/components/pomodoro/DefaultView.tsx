ac49bfb4c310b7fc793a1d8f886f4ad6bcbfbf23
richardSHkim <kshtrue95@gmail.com>
2022-05-11 13:52:03 +0900

    default view

---
+++ src/components/pomodoro/DefaultView.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DefaultView = () => {
+  return (
+    <div>DefaultView</div>
+  )
+}
+
+export default DefaultView
--- src/components/pomodoro/PomodoroPage.tsx
+++ src/components/pomodoro/PomodoroPage.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
+import { useAppSelector } from '../../hooks'
 import '../../styles/PomodoroPage.css'
 
 import CircleView from './CircleView'
+import DefaultView from './DefaultView'
 import TimerModel from './TimerModel'
 
 export const PomodoroPage = () => {
+  const { period } = useAppSelector((state) => state.schedule)
+
   return (
     <>
       <div className='circle-view'>
         <div className='square-view'>
-          <CircleView/>
+          {(period === -1) ? <DefaultView/> : <CircleView/>}
         </div>
       </div>
 
