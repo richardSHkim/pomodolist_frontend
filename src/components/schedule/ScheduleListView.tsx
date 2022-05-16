a460a18cb25064cbd35e82916c9c53988af5be5b
richardSHkim <kshtrue95@gmail.com>
2022-05-16 13:19:15 +0900

    add api GET call

--- src/components/schedule/ScheduleListView.tsx
+++ src/components/schedule/ScheduleListView.tsx
@@ -5,6 +5,19 @@ import ScheduleItem from './ScheduleItem'
 
 const ScheduleListView = () => {
   const { schedule } = useAppSelector((state) => state.schedule)
+
+  // load init schedule from db server
+  useEffect(() => {
+    fetch('http://localhost:8000/api/schedule', {
+      method: 'GET',
+      headers: {'content-type': 'application/json'},
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }, [])
+
   const scheduleList: JSX.Element[] = schedule.map((item, index) => {
     return (
       <div key={index} style={{padding: "5px"}}>
