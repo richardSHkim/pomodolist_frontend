17113bc996847bd4992203ccffe1bde72ea6d00e
richardSHkim <kshtrue95@gmail.com>
2022-05-18 12:22:29 +0900

    responsive with width

--- src/components/pomodoro/DefaultView.tsx
+++ src/components/pomodoro/DefaultView.tsx
@@ -3,8 +3,14 @@ import React from 'react'
 const DefaultView = () => {
   return (
     <span className='no-high'>
-      <p style={{textAlign: 'center', fontWeight: 'bold', fontSize: '100px'}}>
+      <p style={{textAlign: 'center', margin: '0', fontWeight: 'bold', fontSize: '4vw'}}>
         KEEP <br/> CALM
+      </p>
+      <p style={{textAlign: 'center', margin: '0', fontSize: '2vw'}}>
+        AND
+      </p>
+      <p style={{textAlign: 'center', margin: '0', fontWeight: 'bold', fontSize: '4vw'}}>
+        USE <br/> POMODORO
       </p>
     </span>
 
