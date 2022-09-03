8302a193d7d323a96ad8a04079ad882863f82732
richardSHkim <kshtrue95@gmail.com>
2022-09-03 17:11:34 +0900

    chagne api endpoint

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,14 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware(["/api"], {
-      target: 'http://localhost:8000/',
-      changeOrigin: true,
-    })
-  );
-  app.use(
     createProxyMiddleware(["/pyapi"], {
-      target: 'http://localhost:8000/',
+      target: 'http://34.133.64.167:8000/',
       changeOrigin: true,
     })
   );
