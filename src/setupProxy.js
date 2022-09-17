fa6a497d02fc589f4601a262654d627d65cb2030
richardSHkim <kshtrue95@gmail.com>
2022-09-17 21:53:36 +0900

    edit setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,13 +3,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware(["/pyapi"], {
-      target: 'http://pomodoro-fastapi-service:8000/',
+      target: 'http://pomodolist-ml-backend-service:8000/',
       changeOrigin: true,
     })
   );
   app.use(
     createProxyMiddleware(["/api"], {
-      target: 'http://localhost:8000/',
+      target: 'http://pomodolist-backend-service:8001/',
       changeOrigin: true,
     })
   );
